391346a9c30fa6df4165ffa8b47418af3b79d5a0
SozhaTataje <sozhath.04@gmail.com>
2025-05-22 19:12:53 -0500

    Implementación de sucursales

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Navbar from "./components/Navbar";
 import Carrito from "./pages/Carrito";
+import Sucursales from "./pages/Sucursales";
+
+
+
 
 function App() {
   return (
@@ -12,6 +16,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/productos" element={<Products />} />
         <Route path="/carrito" element={<Carrito />} />
+        <Route path="/sucursales" element={<Sucursales />} />
+
       </Routes>
     </BrowserRouter>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,13 @@ const Footer = () => {
           <h3 className="text-lg font-bold">Minimarket Luisa</h3>
           <p className="text-sm">Siempre cerca de ti con los mejores precios.</p>
         </div>
-        <div className="text-sm text-center md:text-right">
+        <div className="text-sm text-center md:text-right mb-4 md:mb-0">
           <p>&copy; {new Date().getFullYear()} Minimarket Luisa. Todos los derechos reservados.</p>
+        </div>
+        <div className="text-sm font-boldtext-center md:text-right">
+          <h3 className=" font-bold mb-1">Contacto</h3>
+          <p>Tel: (+51) 999999999</p>
+          <p>Email: ml@minimarketluisa.com</p>
         </div>
       </div>
     </footer>
